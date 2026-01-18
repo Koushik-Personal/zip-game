8a23166d707e7f2517d148427cc25ad9c992f441
KaushikDas9 <dasbabai2017@gmail.com>
2026-01-18 17:43:14 +0530

    alignment failed again 2

--- src/components/games/NumberShowing.tsx
+++ src/components/games/NumberShowing.tsx
@@ -1,6 +1,12 @@
 export default function NumberShowing({ number }: { number: number }) {
   return (
-    <span className="text-5xl font-bold text-white bg-linear-to-br from-blue-500 to-blue-600 px-8 py-4 rounded-full shadow-lg">
+    <span
+      className="sm:text-2xl md:text-4xl font-bold text-white bg-linear-to-br 
+        from-blue-500 to-blue-600 px-4
+          sm:px-1 md:px-8 py-2
+          sm:py-1 md:py-4
+          rounded-full shadow-lg text-2xl"
+    >
       {number}
     </span>
   );
